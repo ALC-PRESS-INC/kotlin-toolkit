32bf100a8e40dfcc01e39ae5c8aaef6f9e489d93
YoheiDomen-alc <158003686+YoheiDomen-alc@users.noreply.github.com>
2025-12-24 17:00:40 +0900

    Change Pdfium library

--- readium/adapters/pdfium/navigator/build.gradle.kts
+++ readium/adapters/pdfium/navigator/build.gradle.kts
@@ -22,9 +22,9 @@ dependencies {
     api(project(":readium:readium-navigator"))
     api(project(":readium:adapters:pdfium:readium-adapter-pdfium-document"))
     implementation(files("libs/android-pdf-viewer-2.8.2.jar"))
+    implementation(files("libs/PdfiumAndroid-2.0.2-release.aar"))
 
     implementation(libs.androidx.fragment.ktx)
-    implementation(libs.pdfium)
     implementation(libs.timber)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.serialization.json)
